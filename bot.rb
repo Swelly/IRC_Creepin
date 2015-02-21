c7642e95a7dff2c03c7b6ba13d7a6a21880f711f
Swelly <Surfswell17@gmail.com>
2015-02-21 18:10:51 -0500

    swellybot connecting to server

---
+++ bot.rb
@@ -0,0 +1,18 @@
+require 'socket'
+
+server = 'irc.freenode.net'
+port = 6667
+
+nick = 'swellybot'
+channel = "#WatchPeopleCode"
+
+
+s = TCPSocket.open server, port
+s.puts "NICK #{nick}"
+s.puts "User #{nick} 0 * :totally a human!"
+s.puts "JOIN #{channel}"
+
+until s.eof? do
+  msg = s.gets
+  puts msg
+end
---
+++ client.rb
@@ -0,0 +1,3 @@
+get '/' do
+  "First is RUBY\nThen Python\nFinally Ruby IRC Client"
+end
